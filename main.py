3d578a77212d1a3dc7047c7c8275829dec784719
Charlie Smith <charliepsmith09@gmail.com>
2026-06-17 22:01:59 +0100

    AI GPIO integration (can now turn LEDS on and off)

--- main.py
+++ main.py
@@ -1,23 +1,64 @@
 from openai import OpenAI
 import os
+import tiktoken
+import actions
+import json
+
 client = OpenAI(
   base_url="https://openrouter.ai/api/v1",
   api_key=os.getenv("DESK_ROBOT_API_KEY"),
 )
-user_Input = input("Message: ")
-while True:
+#VARIABLES
+callCount = 0
+messages =[]
+ai_config = """
+You are a robot controller.
+Return ONLY valid JSON in this format:
+{
+  "action": "led_on | led_off | none",
+  "response": "a short helpful message to the user"
+}
+Rules:
+- action is for robot control only
+- response is for speaking to the user
+- do not include any extra text outside JSON
+User question: 
+"""
+encoding = tiktoken.get_encoding("cl100k_base")
+
+def send_message():
+  user_Input = input("Message: ")
+
   if user_Input.lower() == "exit":
-    break
-  user_Input = user_Input.lower()
-  response = client.chat.completions.create(
+    return("abort")
+
+  tokens = len(encoding.encode(user_Input))
+
+  print(f"INPUT Token Count: {tokens}")
+  confirm = input("Send message? (y/n): ")
+  if confirm.lower() == "y":
+    response = client.chat.completions.create(
     model="openai/gpt-4o-mini",
     messages=[
       {
         "role": "user",
-        "content": user_Input
+        "content": ai_config + user_Input
       }
-    ]
-  )
-  print(response.choices[0].message.content)
-  user_Input = input("Message: ")
-  
+    ])
+    output =response.choices[0].message.content
+    cmd = json.loads(output)
+    if cmd["action"] == "led_on":
+      actions.led_on()
+    elif cmd["action"] == "led_off":
+      actions.led_off()
+    print(f"AI Response: {cmd['response']}")
+
+
+while True:
+  result = send_message()
+  if result == "abort":
+    break
+  callCount +=1
+  if callCount >= 10:
+    print("Max calls reached. Exiting.")
+    break
